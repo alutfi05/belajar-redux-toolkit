7a2a9608b762d53b6907e686639b903fac88a337
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-06-20 13:58:23 +0700

    adding calculatedTotals reducers in cartSlice

--- vite-starter/src/App.jsx
+++ vite-starter/src/App.jsx
@@ -1,7 +1,17 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { calculateTotals } from './features/cart/cartSlice';
 import Navbar from './components/Navbar';
 import CartContainer from './components/CartContainer';
 
 function App() {
+    const { cartItems } = useSelector((store) => store.cart);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(calculateTotals());
+    }, [cartItems]);
+
     return (
         <main>
             <Navbar />
